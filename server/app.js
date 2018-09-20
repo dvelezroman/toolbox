646f685c9c1444eff670a67bb4e74f3f5dbdfa34
Dario <dvelezroman@gmail.com>
2018-09-20 16:44:33 -0300

    test de api listos

--- server/app.js
+++ server/app.js
@@ -24,3 +24,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(3000, () => console.log('Listening on PORT 3000'));
+
+module.exports = app;
--- server/routes/toolbox.js
+++ server/routes/toolbox.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 router.get('/test', (req, res) => {
-  res.status(200).send('TEST');
+  res.status(200).json({ message: 'Todo Bien' });
 });
 
 router.post('/', (req, res) => {
-  res.status(201).json(req.body);
+  res.status(200).json(req.body);
 })
 
 module.exports = router;
